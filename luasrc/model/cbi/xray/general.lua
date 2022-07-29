ee29562218ed18490540c65eb744e3a84ac0e272
honwen.chan <honwen.chan@gmail.com>
2022-07-29 23:45:21 +0800

    support config:no_dns_injection + no_healthcheck

--- luasrc/model/cbi/xray/general.lua
+++ luasrc/model/cbi/xray/general.lua
@@ -64,6 +64,9 @@ for _, s in ipairs(servers) do o:value(s.name, s.alias) end
 o.default = "nil"
 o.rmempty = false
 
+o = s:option(Flag, "no_dns_injection", translate("Disable DNS Auto-Injection"))
+o.rmempty = false
+
 -- [[ Transparent Proxy ]]--
 s = m:section(TypedSection, "transparent_proxy", translate("Transparent Proxy"))
 s.anonymous = true
@@ -75,6 +78,9 @@ o = s:option(Value, "local_port", translate("Local Port"))
 o.datatype = "port"
 o.default = 1234
 o:depends('enable', '1')
+
+o = s:option(Flag, "no_healthcheck", translate("Disable HealthCheck"))
+o.rmempty = false
 
 -- [[ HTTP Proxy ]]--
 s = m:section(TypedSection, "http_proxy", translate("HTTP Proxy"))
